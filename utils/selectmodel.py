016f97d34bd618ce4511650f2047ffa0c44879d3
Connor Barnsley <ctbarnsley@icloud.com>
2025-02-01 12:59:09 -0500

    typing

--- utils/selectmodel.py
+++ utils/selectmodel.py
@@ -1,22 +1,26 @@
 import torch
+import numpy as np
 
 from models.ResNet import ResNet18
 from models.BasicCNN import ChineseTonesCNN
 from models.MultiTask import MTLNetwork
 
-def SelectModel(model, y_train):
+def SelectModel(model: str, y_train: np.ndarray) -> MTLNetwork | ResNet18 | ChineseTonesCNN | ValueError:
   if model == "mtl":
     num_initials = len(torch.unique(y_train[:, 0]))
     num_finals = len(torch.unique(y_train[:, 1]))
     num_tones = len(torch.unique(y_train[:, 2]))
 
     return MTLNetwork(num_initials=num_initials, num_finals=num_finals, num_tones=num_tones)
+
   elif model == "resnet":
     num_classes = len(torch.unique(y_train)) 
     return ResNet18(num_classes=num_classes)
+
   elif model == "basic":
     num_classes = len(torch.unique(y_train)) 
     return ChineseTonesCNN(num_classes=num_classes)
+
   else:
     raise ValueError(f"Unknown model name: {model}")
 
